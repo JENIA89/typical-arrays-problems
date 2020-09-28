35e013d12dacdee5561d15a414c7942dc2b3e6ca
Evgene Popolamov <popolamov908@gmail.com>
2020-09-29 01:02:17 +0300

    feat: add solve typical arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
+exports.min = function min(array) {
+    if (typeof array == "undefined" || array.length == 0) {
+        return 0;
+    } else {
+        return Math.min(...array);
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (typeof array == "undefined" || array.length == 0) {
+        return 0;
+    } else {
+        return Math.max(...array);
+    }
+};
+exports.avg = function avg(array) {
+    if (typeof array == "undefined" || array.length == 0) {
+        return 0;
+    } else {
+        return array.reduce((a, b) => a + b) / array.length;
+    }
+};
